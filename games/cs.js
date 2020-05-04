505ed5b3ec8566178e86ed50f0d857550b3509d0
chenzengit <ikhoata@gmail.com>
2020-05-04 12:09:30 +0700

    nn

--- games/cs.js
+++ games/cs.js
@@ -1,8 +1,8 @@
 var urlPrefix2 = "https://cdn.jsdelivr.net/gh/mikeylibs/mikeylibs.github.io@1.0.1";
 
-window.url_js_nor = urlPrefix2 + "/games/nor.js";
+// window.url_js_nor = urlPrefix2 + "/games/nor.js";
 
-// window.url_js_nor = "../nor.js";
+window.url_js_nor = "../nor.js";
 
 var script2 = document.createElement('script'); 
 script2.src = window.url_js_nor;
--- games/gb_kiki.js
+++ games/gb_kiki.js
@@ -1,24 +1,30 @@
+// window.url_js_main_kiki = "https://cdn.jsdelivr.net/gh/mikeylibs/mikeylibs.github.io@1.0.2/games/main_kiki.js";
+window.url_js_main_kiki = "../main_kiki.js";
+window.timerOW = 7000;
+
 window.includeJS_GD = function() {
+  console.log("WWWW");
         setTimeout(function () {
           (function(d, s, id) {
                 var js, fjs = d.getElementsByTagName(s)[0];
                 if (d.getElementById(id)) return;
                 js = d.createElement(s);
                 js.id = id;
-                js.src = window.url_js_main;
+                js.src = window.url_js_main_kiki;
                 fjs.parentNode.insertBefore(js, fjs);
           }(document, 'script', 'game' + 'distribution-jssdk'));
         }, window.timerOW);
 };
 
 window.executeGD = function() {
+  console.log("QQQQ");
   (function(d, s, id) {
         var js,
           fjs = d.getElementsByTagName(s)[0];
         if (d.getElementById(id)) return;
         js = d.createElement(s);
         js.id = id;
-        js.src = window.url_js_main;
+        js.src = window.url_js_main_kiki;
         fjs.parentNode.insertBefore(js, fjs);
       })(document, "script", "game" + "distribution-jssdk");
 };
